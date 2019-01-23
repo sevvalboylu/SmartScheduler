c3e9938192d27f93cb5c56c66b3e306e0d8ff943
selineyp <eyupogluselin@sabanciuniv.edu>
2019-01-24 00:01:54 +0300

    implementing nav drawer w/ toggle - in progress

--- app/src/main/java/com/sabanciuniv/smartschedule/app/NavigationActivity.java
+++ app/src/main/java/com/sabanciuniv/smartschedule/app/NavigationActivity.java
@@ -33,24 +33,36 @@ public class NavigationActivity extends AppCompatActivity
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+           //     this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //drawer.setDrawerListener(toggle);
+
+
+        NavigationView navigationView = (NavigationView)
+                findViewById(R.id.nav_view);
+
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =
+                (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
