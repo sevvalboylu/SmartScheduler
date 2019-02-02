368120f40132802ac98c34ed0bfac8b56b846530
selineyp <eyupogluselin@sabanciuniv.edu>
2019-02-02 23:57:00 +0300

    added new searchactivity

--- app/src/main/java/com/sabanciuniv/smartschedule/app/AddTask.java
+++ app/src/main/java/com/sabanciuniv/smartschedule/app/AddTask.java
@@ -3,23 +3,34 @@ package com.sabanciuniv.smartschedule.app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import com.firebase.client.Firebase;
+import android.widget.Toast;
 
-import com.google.firebase.*;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.quickstart.database.R;
-import com.google.firebase.quickstart.database.java.models.Post;
-import com.google.firebase.quickstart.database.java.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.yandex.mapkit.geometry.Point;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddTask extends AppCompatActivity {
 
+    private static final String TAG = "AddTask";
+    private static final String REQUIRED = "Required";
     private DatabaseReference mDatabase;
+    private EditText mTitleField;
+    private Spinner spinner1, spinner2;
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +38,106 @@ public class AddTask extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mTitleField = findViewById(R.id.fieldTitle);
-        mBodyField = findViewById(R.id.fieldBody);
-        mSubmitButton = findViewById(R.id.fabSubmitPost);
+        mTitleField = findViewById(R.id.mytaskTitle);
+
+        mSubmitButton = findViewById(R.id.addTask);
+
         //get the spinner from the xml.
-        Spinner dropdown = findViewById(R.id.spinner1);
+        Spinner dropdown = findViewById(R.id.impspin);
+        addListenerOnSpinnerItemSelection();
         //create a list of items for the spinner.
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitTask();
+            }
+        });
         String[] items = new String[]{"1", "2", "three"};
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
        //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-    //set the spinners adapter to the previously created one.
+       //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
 
         //go to map or dropdown list of most frequent places
     }
 
+    private void submitTask() {
+        final String title = mTitleField.getText().toString();
+       // final Point location =
+
+        // Title is required
+        if (TextUtils.isEmpty(title)) {
+            mTitleField.setError(REQUIRED);
+            return;
+        }
+
+        // Disable button so there are no multi-posts
+        //setEditingEnabled(false);
+        Toast.makeText(this, "Submitting the task...", Toast.LENGTH_SHORT).show();
+
+        // [START single_value_read]
+        final String userId = getUid();
+        mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        User user = dataSnapshot.getValue(User.class);
+
+                        // [START_EXCLUDE]
+                        if (user == null) {
+                            // User is null, error out
+                            Log.e(TAG, "User " + userId + " is unexpectedly null");
+                            Toast.makeText(AddTask.this,
+                                    "Error: could not fetch user.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Write new post
+                            addnewTask(userId, user.username, title, location);
+                        }
+
+                        // Finish this Activity, back to the stream
+                        //setEditingEnabled(true);
+                        finish();
+                        // [END_EXCLUDE]
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        // [START_EXCLUDE]
+                        //setEditingEnabled(true);
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END single_value_read]
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.impspin);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+    private void addnewTask(String userId, String username, String title, Point location) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("").push().getKey();
+        Task task = new Task(userId, title, location);
+        Map<String, Object> postValues = task.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+    }
     public void goToMap(View view)
     {
         Intent intent = new Intent(AddTask.this, MapViewActivity.class);
         startActivity(intent);
     }
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
 }
---
+++ app/src/main/java/com/sabanciuniv/smartschedule/app/Task.java
@@ -0,0 +1,41 @@
+package com.sabanciuniv.smartschedule.app;
+
+import com.yandex.mapkit.geometry.Point;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// [START post_class]
+@IgnoreExtraProperties
+public class Task {
+
+    public String uid;
+    public String title;
+    public Point location;
+
+    public Task() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+    }
+
+    public Task(String uid, String title, Point location) {
+        this.uid = uid;
+        this.title = title;
+        this.location = location;
+    }
+
+    // [START post_to_map]
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("title", title);
+        result.put("location", location);
+
+        return result;
+    }
+    // [END post_to_map]
+
+}
+// [END post_class]
