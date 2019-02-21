63b73349f5a0d647e7b6e10956398e88f62aa6a3
cetsen <senacetin96@gmail.com>
2019-02-21 21:40:12 +0300

    Binded date and time switches with their pickers

--- app/src/main/java/com/sabanciuniv/smartschedule/app/AddTask.java
+++ app/src/main/java/com/sabanciuniv/smartschedule/app/AddTask.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -31,6 +34,9 @@ public class AddTask extends AppCompatActivity {
     private TextView mLocationField;
     private Spinner spinner1, freqLocationSpinner;
     private Button mSubmitButton;
+    private Switch mPickTimeSwitch, mPickDateSwitch, mAllDaySwitch;
+    private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
     private String lvl;
     //private static final Point location = new Point(41.0082, 28.9784); //should not be static, change later
     private final String location = new String();
@@ -48,6 +54,55 @@ public class AddTask extends AppCompatActivity {
         mLocationField = findViewById(R.id.address);
 
         mSubmitButton = findViewById(R.id.addTask);
+
+        mPickDateSwitch = findViewById(R.id.pickDateSwitch);
+
+        mPickTimeSwitch = findViewById(R.id.pickTimeSwitch);
+
+        mAllDaySwitch = findViewById(R.id.allDaySwitch);
+        mAllDaySwitch.setVisibility(View.GONE);
+
+        mDatePicker = findViewById(R.id.datePicker1);
+        mDatePicker.setVisibility(View.GONE);
+
+        mTimePicker = findViewById(R.id.timePicker1);
+        mTimePicker.setVisibility(View.GONE);
+
+        mPickDateSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPickDateSwitch.isChecked()) {
+                    mDatePicker.setVisibility(View.VISIBLE);
+                } else if(!mPickDateSwitch.isChecked()) {
+                    mDatePicker.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        mPickTimeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPickTimeSwitch.isChecked()) {
+                    mTimePicker.setVisibility(View.VISIBLE);
+                    mAllDaySwitch.setVisibility(View.VISIBLE);
+                } else if (!mPickDateSwitch.isChecked()) {
+                    mTimePicker.setVisibility(View.GONE);
+                    mAllDaySwitch.setVisibility(View.GONE);
+                    mAllDaySwitch.setChecked(false);
+                }
+            }
+        });
+
+        mAllDaySwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAllDaySwitch.isChecked()) {
+                    mTimePicker.setVisibility(View.GONE);
+                } else if(!mAllDaySwitch.isChecked()) {
+                    mTimePicker.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.importanceSpinner);
